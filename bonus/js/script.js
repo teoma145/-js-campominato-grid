0af8b23711d595a0964534e41ebfd7b20a49b653
teoma145 <matteodark145@yahoo.it>
2023-10-18 17:24:42 +0200

    difficoltà

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -6,18 +6,30 @@ console.log(btn)
 btn.addEventListener('click',function(){
   let sceltadifficoltà = document.getElementById('Difficoltà').value;
   console.log(sceltadifficoltà)
-  
+  let numeroquadratini;
+  if(sceltadifficoltà ==='facile'){
+      numeroquadratini = 100;
+  }
+  else if(sceltadifficoltà==='medio'){
+     numeroquadratini = 81;
+  }
+  else{
+     numeroquadratini=49;
+  }
    const squarebox = document.getElementById('squarebox')
-   const numeroquadratini = 100;
+   
    for(let i = 0; i < numeroquadratini;i++){
-    let square = boxadd(i);
+    let square = boxadd(i,numeroquadratini);
     squarebox.append(square);
    }
 })
 
-function boxadd(quadratinoattuale){
+function boxadd(quadratinoattuale,numsquare){
+    squarewidth= Math.sqrt(numsquare)
     const square = document.createElement('div')
     square.classList.add('box')
+    square.style.width=`calc(100% / ${squarewidth})`
+    square.style.height=`calc(100% / ${squarewidth})`
     square.innerHTML=quadratinoattuale + 1;
     square.addEventListener('click',function(){
       square.classList.add('clicksquare')
