eac741fdd913cc87da5014c6ec5d3b37391ba07a
teoma145 <matteodark145@yahoo.it>
2023-10-18 16:52:40 +0200

    bonus-start

---
+++ bonus/js/script.js
@@ -0,0 +1,22 @@
+
+const btn=document.querySelector('button')
+console.log(btn)
+
+btn.addEventListener('click',function(){
+   const squarebox = document.getElementById('squarebox')
+   const numeroquadratini = 100;
+   for(let i = 0; i < numeroquadratini;i++){
+    let square = boxadd(i);
+    squarebox.append(square);
+   }
+})
+
+function boxadd(quadratinoattuale){
+    const square = document.createElement('div')
+    square.classList.add('box')
+    square.innerHTML=quadratinoattuale + 1;
+    square.addEventListener('click',function(){
+      square.classList.add('clicksquare')
+    })
+    return square
+}
